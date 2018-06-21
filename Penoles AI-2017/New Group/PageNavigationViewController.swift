c5de193ae73487651a82549959ba030f24dadc3c
alex <alex@nuevecubica.net>
2018-06-20 19:48:21 -0500

    all pages --> needs popups

--- Penoles AI-2017/New Group/PageNavigationViewController.swift
+++ Penoles AI-2017/New Group/PageNavigationViewController.swift
@@ -38,9 +38,8 @@ class PageNavigationViewController: UIViewController {
     var sections:[Section] = [
         Section(name: "Portada", pages: [
            // ImagePage(imageName: "portada"),
-            CoverPage(theBg: "cover_bg", theFront: "cover_title", theTitle: "cover_front")
+            CoverPage(theBg: "cover_bg", theFront: "cover_title", theTitle: "cover_front"),
             ]),
-        
         Section(name: "Perfil Corporativo", pages: [
             ImagePage(imageName: "perfil_corp_pag1"),
             ImagePage(imageName: "perfil_corp_pag2"),
@@ -52,7 +51,79 @@ class PageNavigationViewController: UIViewController {
             ImagePage(imageName: "modelo_pag3"),
             ImagePage(imageName: "modelo_pag4"),
             ]),
-        
+        Section(name: "Informe anual", pages: [
+            ImagePage(imageName: "inf_anual_pag1"),
+            ]),
+        Section(name: "Informe del Director", pages: [
+            ImagePage(imageName: "inf_director_pag1"),
+            ]),
+        Section(name: "Resumen de operaciones", pages: [
+            CoverPage(theBg: "ro_cover_bg", theFront: "ro_cover_title", theTitle: "ro_cover_front"),
+            ImagePage(imageName: "res_operacion_pag1"),
+            ImagePage(imageName: "res_operacion_pag2"),
+            ImagePage(imageName: "res_operacion_pag3"),
+            ImagePage(imageName: "res_operacion_pag4"),
+            ImagePage(imageName: "res_operacion_pag5"),
+            res_operacion_6Page(imageName: "res_operacion_pag6"),
+            res_operacion_7Page(imageName: "res_operacion_pag7"),
+            res_operacion_8Page(imageName: "res_operacion_pag8"),
+            ImagePage(imageName: "res_operacion_pag9"),
+            ImagePage(imageName: "res_operacion_pag10"),
+            ImagePage(imageName: "res_operacion_pag11"),
+            ImagePage(imageName: "res_operacion_pag12"),
+            ImagePage(imageName: "res_operacion_pag13"),
+            res_operacion_14Page(imageName: "res_operacion_pag14"),
+            res_operacion_15Page(imageName: "res_operacion_pag15"),
+            ImagePage(imageName: "res_operacion_pag16"),
+            ImagePage(imageName: "res_operacion_pag17"),
+            ImagePage(imageName: "res_operacion_pag18"),
+            res_operacion_19Page(imageName: "res_operacion_pag19"),
+            res_operacion_20Page(imageName: "res_operacion_pag20"),
+            ImagePage(imageName: "res_operacion_pag21"),
+            
+            ]),
+        Section(name: "Energía y Tecnológica", pages: [
+            CoverPage(theBg: "et_cover_bg", theFront: "et_cover_front", theTitle: "et_cover_title"),
+            ImagePage(imageName: "energia_tec_pag1"),
+            ImagePage(imageName: "energia_tec_pag2"),
+            ]),
+        Section(name: "Nuestra gente", pages: [
+            CoverPage(theBg: "ng_cover_bg", theFront: "ng_cover_front", theTitle: "ng_cover_title"),
+            ImagePage(imageName: "nuestra_gente_pag1"),
+            ]),
+        Section(name: "Gobierno Corporativo", pages: [
+            CoverPage(theBg: "gc_cover_bg", theFront: "gc_cover_front", theTitle: "gc_cover_title"),
+            ImagePage(imageName: "gob_corp_pag1"),
+            ImagePage(imageName: "gob_corp_pag2"),
+            ]),
+        Section(name: "Ejecutivos", pages: [
+            ImagePage(imageName: "ejecutivos_pag1"),
+            ]),
+        Section(name: "Equipo directivo", pages: [
+            ImagePage(imageName: "ejecutivos_pag1"),
+            ]),
+        Section(name: "Consejo de Administración", pages: [
+            ImagePage(imageName: "ejecutivos_pag1"),
+            ]),
+        Section(name: "Análisis y discusión de resultados ", pages: [
+            ImagePage(imageName: "analisis_res_pag1"),
+            ImagePage(imageName: "analisis_res_pag2"),
+            ImagePage(imageName: "analisis_res_pag3"),
+            ImagePage(imageName: "analisis_res_pag4"),
+            ImagePage(imageName: "analisis_res_pag5"),
+            ImagePage(imageName: "analisis_res_pag6"),
+            ImagePage(imageName: "analisis_res_pag7"),
+            ImagePage(imageName: "analisis_res_pag8"),
+            ]),
+        Section(name: "Informe del comite de auditoría", pages: [
+            ImagePage(imageName: "inf_comite_pag1"),
+            ]),
+        Section(name: "Información para accionistas", pages: [
+            ImagePage(imageName: "inf_accionistas_pag1"),
+            ]),
+        Section(name: "Contra portada", pages: [
+            ImagePage(imageName: "back_cover"),
+            ]),
         
         ]
 
---
+++ Penoles AI-2017/New Group/res_operacion_14.swift
@@ -0,0 +1,57 @@
+//
+//  res_operacion_14.swift
+//  Penoles AI-2017
+//
+//  Created by alex vaught on 6/20/18.
+//  Copyright © 2018 nuevecubica. All rights reserved.
+//
+
+import UIKit
+
+class res_operacion_14Page: ImagePage {
+    override var viewController: UIViewController {
+        return res_operacion_14ViewController(image: UIImage(named: imageName)!)
+    }
+}
+
+class res_operacion_14ViewController: PageWithButtonsViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let imageScale = view.bounds.width / imageView.image!.size.width
+        
+        let icon = "plus"
+        
+        
+        //*************************************
+        //**************************************
+        // Button Cifras financieras sobresalientes
+        //**************************************
+        let button_cfs = UIButton(frame: CGRect())
+        button_cfs.addTarget(self, action: #selector(button_cfs_Pressed(_:)), for: .touchUpInside)
+        button_cfs.setImage(UIImage(named : icon), for: UIControlState.normal)
+        imageView.addSubview(button_cfs)
+        button_cfs.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Image coords divided by 3
+        NSLayoutConstraint.activate([
+            button_cfs.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 854.9 * imageScale),
+            button_cfs.topAnchor.constraint(equalTo: imageView.topAnchor, constant:  454.0 * imageScale)
+            ])
+        //*************************************
+        
+        
+    }
+    
+    //*****************************************
+    // PRESS FUNCTIONS
+    //*****************************************
+    
+    @IBAction func button_cfs_Pressed(_ sender: UIButton) {
+        showImagePopup(image:#imageLiteral(resourceName: "mn_CifrasFinancierasSobresalientes"))
+        //showVideoPopup(videoName: "video")
+    }
+    
+}
